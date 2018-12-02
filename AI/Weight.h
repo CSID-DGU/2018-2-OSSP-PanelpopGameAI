bb6a6b55c5401d6ded78f5b241cd88999601b5aa
bluelize <45114008+bluelize@users.noreply.github.com>
2018-12-03 03:10:37 +0900

    add setter functions

--- AI/Weight.h
+++ AI/Weight.h
@@ -2,8 +2,8 @@
 /*
  * Weight.h
  *
- *  Modified on: Nov 14, 2018
- *      Author: limjuhee
+ *  Modified on: Dec 3, 2018
+ *      Author: blue
  */
 
 #ifndef AI_WEIGHT_H_
@@ -34,11 +34,21 @@ public:
 	// Constructor
 	Weight(int _ID, double _verticalBlockWeight, double _horizontalBlockWeight,
 		double _isMeetGarbageWeight, double _isExHighWeight, double isLT2);
+	
+	//getter
 	double get_verticalBlockWeight();
 	double get_horizontalBlockWeight();
 	double get_isMeetGarbageWeight();
 	double get_isExHighWeight();
 	double get_isLT2();
+
+	//setter
+	void set_verticalBlockWeight(double _verticalBlockWeight);
+	void set_horizontalBlockWeight(double _horizontalBlockWeight);
+	void set_isMeetGarbageWeight(double _isMeetGarbageWeight);
+	void set_isExHighWeight(double _isExHighWeight);
+	void set_isLT2(double _isLT2);
+
 	int get_ID();
 	int get_score();
 }
