5f49ea7209921265b8201ca279205ae914b06d4f
bluelize <45114008+bluelize@users.noreply.github.com>
2018-12-04 00:53:36 +0900

    add map<> for pairing

--- AI/GeneticAlgorithm.h
+++ AI/GeneticAlgorithm.h
@@ -2,7 +2,7 @@
 /*
  * GeneticAlgorithm.h
  *
- *  Created on: Nov 13, 2018
+ *  Modified on: Dec 4, 2018
  *      Author: blue
  */
 
@@ -11,6 +11,7 @@
 #include "Weight.h"
 #include <vector>
 #include <string>
+#include <map>
 
 using namespace std;
 
@@ -24,15 +25,17 @@ private:
 	// vector <Weight> it;
 	// indicator of Generation
 	int Generation;
-	// size of superior children
-	int poolsize;
-
+    // size of superior children
+	// int poolsize;
+	// pair of children which are selected
+	map<int, int> pair;
+	
 public:
 
 	// Constructor : Generating parent
 	GeneticAlgorithm();
-	// crossover children considering mutation
-	void crossOver(string* chromo1, string * chromo2);
+	// crossover children
+	void crossOver(string * chromo1, string * chromo2);
 	// Destructors
 	~GeneticAlgorithm();
 	// generate random number between upperLimit and lowLimit
@@ -42,12 +45,12 @@ public:
 	// running GA
 	void runGA();
 	// using roulette wheel selection
-	int selection(int totalFitness, Weight* ptr);
+	int selection(int totalFitness, vector<Weight> list);
 	// mutation
 	void mutation(string * chromo);
 	// convert double value weights to binary 
 	string double2bin(double real);
 	// convert binary bits to double value weights
-	double bin2double(string * ptr);
+	double bin2double(string ptr);
 }
 #endif /* AI_GENETICALGORITHM_H _ */
