953730e8e8996c77c54880fb9eca44836db55e95
yoonsy97 <43856086+yoonsy97@users.noreply.github.com>
2018-12-08 14:14:29 +0900

    Update AITrainingGameState.h
    code 정리

--- States/AITrainingGameState.h
+++ States/AITrainingGameState.h
@@ -1,36 +1,44 @@
 
 /* 
- * File:   AIGameState.h
+ * File:   AITrainingGameState.h
  * Author: ysy
  *
  * Created on 2018/12/7
  */
 
-#ifndef AITRAININGGAMESTATE_H
-#define AITRAININGGAMESTATE_H
+#ifndef STATES_AITRAININGGAMESTATE_H
+#define STATES_AITRAININGGAMESTATE_H
+
+#include "../Game/EndlessGame.h"
+#include "../Rendering/EndlessGameRenderer.h"
+#include "State.h"
+#include "GameState.h"
 
 #include "../AI/AIBoardController.h"
-#include "GameState.h"
 
 #include "../AI/GeneticAlgorithm.h"
 
-extern vector<Weight> list;
 
-int maxScore;
+
+
+extern vector<Weight> weightList;
+
 
 class AITrainingGameState : public GameState{
 public:
 	AITrainingGameState();
-	void tick();
+	void tick() override;
 	
 protected:
 	GeneticAlgorithm* geneticAlgorithm;
-	std::vector<AIBoardController*> _opponentBoardcontollers;
+//	std::vector<AIBoardController*> _opponentBoardcontollers;
 	AIBoardController* temp;
 	int count = 0;
+	int maxScore;
+
 	
 private:
 
 };
 
-#endif /* AITRAININGGAMESTATE_H */
+#endif /* STATES_AITRAININGGAMESTATE_H */
