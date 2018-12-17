3f623afbbd2c3f9eb4db5787aea36687f8d136c1
bluelize <45114008+bluelize@users.noreply.github.com>
2018-12-18 01:25:14 +0900

    Update GeneticAlgorithm.h

--- AI/GeneticAlgorithm.h
+++ AI/GeneticAlgorithm.h
@@ -2,14 +2,14 @@
 /*
  * GeneticAlgorithm.h
  *
- *  Modified on: Dec 9, 2018
+ *  Modified on: Dec 18, 2018
  *      Author: blue
  */
 
 #ifndef AI_GENETICALGORITHM_H_
 #define AI_GENETICALGORITHM_H_
 #include "Weight.h"
-#include <Vector>
+#include <vector>
 #include <string>
 #include <map>
 
@@ -19,25 +19,28 @@ using namespace std;
 // list of weight
 extern vector<Weight> weightList;
 // global variable
-static int Generation = 0;
+static int Generation = 10;
 
 class GeneticAlgorithm {
-	
+
 public:
 
+	int c1 = 0;
+	int c2 = 0;
+	double totalScore = 0;
 	// Constructor : Generating parent
 	GeneticAlgorithm();
-	
+
 	// generate random number between upperLimit and lowLimit
 	double random(int upperLimit, int lowerLimit);
 	// write weights to file
-	void writeWeightToFile();	
+	void writeWeightToFile();
 	// using roulette wheel selection
-	int selection(int index, vector <Weight> weightList);
+	void selection();
 	// crossOver
-	double* crossOver(double chromo1, double chromo2);
+	void crossOver();
 	// mutation
-	double mutation(double chromo);
+	double mutation(double w);
 	// running GA
 	void runGA();
 	// Destructors
